cfdfd2a6c3589286e17deadec47b3eaca8fbec0d
Demi-LL <liuliunft@gmail.com>
2022-01-20 19:40:09 +0800

    feat: 加入功能，提供使用者輸入
    1. 可輸入主網址、合約地址、要儲存的資料夾路徑
    2. TODO: 自動生成不存在的目標資料夾

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ import cloudscraper
 from random import choice
 from bs4 import BeautifulSoup
 
+domain = input('Please input the contract domain you want to fetch (default is etherscan): ')
+
+if len(domain.strip()) == 0:
+  domain = 'https://etherscan.io/address/'
+  
+contract_address = input('Contract Address: ')
+
+save_dir = input('Input the condirectory you want to save result: ')
+
+if len(save_dir.strip()) == 0:
+  save_dir = './'
+
 user_agent = ['chrome', 'firefox']
 platform = ['linux', 'windows', 'darwin']
 scraper = cloudscraper.create_scraper(browser={'browser': choice(user_agent), 'platform': choice(platform)})
-res = scraper.get('https://etherscan.io/address/0x71c4658acc7b53ee814a29ce31100ff85ca23ca7#code')
+res = scraper.get('{}{}'.format(domain, contract_address))
 html_content = res.text
 
 soup = BeautifulSoup(html_content, 'lxml')
@@ -22,6 +34,6 @@ for filename, code in zip(filenames, codes):
     print('[{}] occured error.'.format(filename.strip()))
   else:
     pure_filename = regex_result.group(1)
-    with open(pure_filename, 'w+') as file:
+    with open('{}/{}'.format(save_dir.rstrip('/'), pure_filename), 'w+') as file:
       file.write(code)
       file.close()
